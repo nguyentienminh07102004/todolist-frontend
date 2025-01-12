ed60cce8e77eaea1da8bfdd87a77acf5d3a2101d
Nguyen Tien Minh <nguyentienminhntm07102004@gmail.com>
2025-01-12 18:12:09 +0700

    Update

--- src/service/login.ts
+++ src/service/login.ts
@@ -16,7 +16,7 @@ export const LoginUser = async (
 
 export const loginWithGoogle = (): void => {
 	const clientId: string = import.meta.env.VITE_clientId;
-	const redirect_uri: string = "https://todolist-frontend-iota.vercel.app/?social=google";
+	const redirect_uri: string = "https://todolist-frontend-fel9jqayt-nguyen-tien-minhs-projects.vercel.app/?social=google";
 	const scope: string =
 		"https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile";
 	const url = `https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=${redirect_uri}
